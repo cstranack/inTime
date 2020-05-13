e95ce3d26b19660ed8667dacb4ce9f9536e49939
Claire Stranack <claire@stranack.co.uk>
2020-05-13 17:22:32 +0100

    inital document set up

--- server.js
+++ server.js
@@ -1,26 +1,20 @@
 var express = require('express');
 var app = express();
+var handlebars = require('express-handlebars');
 
-//web application getting something from the server
-//req = request //res = response //=>'fat arrow' = function
-app.get('/', (req, res) => {
-    res.send('This is the homepage :D ');
+app.set('view engine', 'hbs');
+app.engine('hbs', handlebars({
+    layoutsDir: __dirname + '/views/layouts',
+    extname: 'hbs'
+}));
 
+//web application requesting login page
+//req = request // res = response // =>'fat arrow' = function
+app.get('/', (req, res) =>{
+    res.render('index', {layout: 'main' });
 });
-//second page
-app.get('/about', (req, res) => {
-    res.send('This is the about page :D ');
-
-});
-
-//third page
-app.get('/contact', (req, res) => {
-    res.send('This is the contact page :D ');
-
-});
-
 
 //listening for requests on port 3000
 app.listen(3000,() => {
-    console.log(' Server listening on port 3000 :) ');
+    console.log('Server listening on port 3000 :) ');
 });
